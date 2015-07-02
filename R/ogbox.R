d2b366df06b3ccd162c0a381bb2661c317cc906a
Ogan Mancarci <ogan.mancarcii@gmail.com>
2015-07-01 20:44:44 -0700

    ogbox new circle for igraph not sure if it'll work

--- R/ogbox.R
+++ R/ogbox.R
@@ -487,3 +487,37 @@ trimWS <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 #' @export
 coVar = function(x) ( 100*sd(x)/mean(x) )
+
+# bordered circles for igraph 
+# http://lists.gnu.org/archive/html/igraph-help/2013-03/msg00030.html
+#' @export
+mycircle <- function(coords, v=NULL, params) {
+    vertex.color <- params("vertex", "color")
+    if (length(vertex.color) != 1 && !is.null(v)) {
+        vertex.color <- vertex.color[v]
+    }
+    vertex.size  <- 1/200 * params("vertex", "size")
+    if (length(vertex.size) != 1 && !is.null(v)) {
+        vertex.size <- vertex.size[v]
+    }
+    vertex.frame.color <- params("vertex", "frame.color")
+    if (length(vertex.frame.color) != 1 && !is.null(v)) {
+        vertex.frame.color <- vertex.frame.color[v]
+    }
+    vertex.frame.width <- params("vertex", "frame.width")
+    if (length(vertex.frame.width) != 1 && !is.null(v)) {
+        vertex.frame.width <- vertex.frame.width[v]
+    }
+    
+    mapply(coords[,1], coords[,2], vertex.color, vertex.frame.color,
+           vertex.size, vertex.frame.width,
+           FUN=function(x, y, bg, fg, size, lwd) {
+               symbols(x=x, y=y, bg=bg, fg=fg, lwd=lwd,
+                       circles=size, add=TRUE, inches=FALSE)
+           })
+}
+
+#' @export
+add.vertex.shape("fcircle", clip=igraph.shape.noclip,
+                 plot=mycircle, parameters=list(vertex.frame.color=1,
+                                                vertex.frame.width=1))
