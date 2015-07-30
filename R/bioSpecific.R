8b119fd38dca70f890d671e63c57ef16d989db04
Ogan Mancarci <ogan.mancarcii@gmail.com>
2015-07-30 16:42:05 -0700

    removed extra and old sepExpr

--- R/bioSpecific.R
+++ R/bioSpecific.R
@@ -5,37 +5,6 @@
 # dfs in one go
 #' @export
 sepExpr = function(allDataPre){
-    for (i in 1:ncol(allDataPre)){
-        if ('double'==typeof(allDataPre[,i])){
-            expBound = i
-            break
-        }
-    }
-    geneData = allDataPre[,1:(expBound-1)]
-    exprData = allDataPre[,expBound:ncol(allDataPre)]
-    return(list(geneData,exprData))
-}
-
-
-# common read-write functions for data analysis -----
-#' @export
-write.design = function(x, file){
-    write.table(x,file= file, sep = '\t', quote=F, row.names = F)
-}
-
-#' @export
-read.design  = function(x){
-    read.table(x,header=T,sep='\t',stringsAsFactors=F,quote="")
-}
-
-#' @export
-read.exp = function(x){
-    read.csv(x,header = T,stringsAsFactors=F)
-}
-
-# seperates expression values from gene information
-#' @export
-sepExpr = function(allDataPre){
     if (class(allDataPre)[1] =='data.frame'){
         for (i in 1:ncol(allDataPre)){
             if ('double'==typeof(allDataPre[,i])){
@@ -57,4 +26,21 @@ sepExpr = function(allDataPre){
         exprData = allDataPre[,expBound:ncol(allDataPre), with = F]
         return(list(geneData,exprData))
     }
+}
+
+
+# common read-write functions for data analysis -----
+#' @export
+write.design = function(x, file){
+    write.table(x,file= file, sep = '\t', quote=F, row.names = F)
+}
+
+#' @export
+read.design  = function(x){
+    read.table(x,header=T,sep='\t',stringsAsFactors=F,quote="")
+}
+
+#' @export
+read.exp = function(x){
+    read.csv(x,header = T,stringsAsFactors=F)
 }
