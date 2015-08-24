8a87275f58887ac2a15244e3bb256ff8d759fb01
OganM <ogan.mancarcii@gmail.com>
2015-08-24 11:38:48 -0700

    another one for testing

--- tests/testthat/testing.R
+++ tests/testthat/testing.R
@@ -1,8 +1,8 @@
 context('general error check')
 agzinaSictigimin = 1
-test_that('purge unable to delete objects', {
-    ogbox::purge()
-    expect_error(print(agzinaSictigimin),"object 'agzinaSictigimin' not found")
+test_that("trimNAs can't trim NAs", {
+    a = c(1,2,NA)
+    expect_that(ogbox::trimNAs(a),equals(c(1,2)))
 })
 
 
