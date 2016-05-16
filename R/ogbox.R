9df57c0b84d827c1bff608adf80645de15a12b94
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-05-16 15:48:57 -0700

    ogboxvio can change its data

--- R/ogbox.R
+++ R/ogbox.R
@@ -534,9 +534,9 @@ mycircle <- function(coords, v=NULL, params) {
 }
 
 #' @export
-geom_ogboxvio = function(){
-   list(geom_violin(color="#C4C4C4", fill="#C4C4C4"),
-        geom_boxplot(width=0.1,fill = 'lightblue'), 
+geom_ogboxvio = function(data=NULL){
+   list(geom_violin(color="#C4C4C4", fill="#C4C4C4",data=data),
+        geom_boxplot(width=0.1,fill = 'lightblue',data=data), 
         theme_bw(),
         theme(axis.text.x  = element_text(size=25),
               axis.title.y = element_text(vjust=0.5, size=25),
