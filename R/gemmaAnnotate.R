a4cc08b76c83a6dbf16c8e69104ebe8eb95b7250
Ogan Mancarci <ogan.mancarcii@gmail.com>
2016-09-19 13:58:27 -0700

    getGemmaAnnot doesn't try to get the file if it exists

--- R/gemmaAnnotate.R
+++ R/gemmaAnnotate.R
@@ -4,15 +4,23 @@
 
 
 #' @export
-getGemmaAnnot = function(chipName,chipFile,annotType = c('bioProcess','noParents','allParents')){
+getGemmaAnnot = function(chipName,chipFile,annotType = c('bioProcess','noParents','allParents'), 
+                         overwrite=FALSE){
     annotType = match.arg(annotType)
     if (annotType == 'allParents'){
         annotType = ''
+    } else{
+        annotType = paste0('_',annotType)
     }
-    download.file(paste0('http://chibi.ubc.ca/microannots/',chipName,'_',annotType,'.an.txt.gz'),
+    if(file.exists(chipFile) & !overwrite){
+        print('the file already exists. not overwriting')
+        return(FALSE)
+    }
+    download.file(paste0('http://chibi.ubc.ca/microannots/',chipName,annotType,'.an.txt.gz'),
                   paste0(chipFile,'.gz'))
     
     system(paste0('gunzip ', chipFile,'.gz'))
+    return(TRUE)
 }
 
 # for affy package
