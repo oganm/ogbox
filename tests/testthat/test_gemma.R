c87e8f5514191af155eea992cb03e62662ede425
OganM <ogan.mancarcii@gmail.com>
2017-02-17 18:05:19 -0800

    gemma test fix

--- tests/testthat/test_gemma.R
+++ tests/testthat/test_gemma.R
@@ -6,10 +6,10 @@ test_that('getGemmaAnnot', {
     if(file.exists(temp)){
         file.remove(temp)
     }
-    expect_that(getGemmaAnnot('GPL1261',temp),testthat::equals(TRUE))
+    expect_that(getGemmaAnnot('GPL8',temp),testthat::equals(TRUE))
     
-    expect_that(getGemmaAnnot('GPL1261',temp),testthat::equals(FALSE))
+    expect_that(getGemmaAnnot('GPL8',temp),testthat::equals(FALSE))
     
-    expect_that(getGemmaAnnot('GPL1261',temp,overwrite = TRUE),testthat::equals(TRUE))
+    expect_that(getGemmaAnnot('GPL8',temp,overwrite = TRUE),testthat::equals(TRUE))
     
 })
