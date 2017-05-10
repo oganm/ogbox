732f8f385d1c7dde5263634153d02755421980e8
OganM <ogan.mancarcii@gmail.com>
2017-05-10 14:38:01 -0700

    prettifyLatexKable added

--- R/ogbox.R
+++ R/ogbox.R
@@ -604,4 +604,35 @@ qNormToValues = function(x,values,uniquelyOrdered = FALSE ,...){
     }
 }
 
+#' Prettify a latex table from knitr
+#' @description Makes latex tables coming out of knitr prettier. Requires booktabs latex package on the file
+#' @export
+prettifyLatexKable = function(table,columnLines = FALSE){
+    tableLines = table  %>% 
+        stringr::str_replace_all(pattern = '\n\\\\hline','') %>%
+        stringr::str_split('\n') %>% {.[[1]]} 
+    
+    tableTop = tableLines %>% 
+    {grep(pattern = '\\\\begin\\{tabular\\}',x=.)}
+    
+    if(!columnLines){
+        tableLines[tableTop] %<>% stringr::str_replace_all(pattern='\\|','')
+    }
+    
+    tableBottom = tableLines %>% 
+    {grep(pattern = '\\\\end\\{tabular\\}',x=.)}
+    
+    newTableLines = c(tableLines[1:tableTop],
+                      '\\toprule',
+                      tableLines[tableTop+1],
+                      '\\midrule',
+                      tableLines[(tableTop+1):(tableBottom-1)],
+                      '\\bottomrule',
+                      tableLines[tableBottom:length(tableLines)])
+    
+    outTable = newTableLines %>% paste(collapse ='\n')
+    attributes(outTable) = attributes(table)
+    return(outTable)
+}
+
 
